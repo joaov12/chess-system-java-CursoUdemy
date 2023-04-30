bece97e252554fb9c57698b5d6b6f58da550f47f
João Vitor Farias Soares <jvfs12th@gmail.com>
2023-04-30 10:43:05 -0300

    Começando a implementar o tabuleiro e as peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-
-		
-		Position pos = new Position(3,5);
-		System.out.println(pos);
+				
+	Board board = new Board(8, 8);
 	}
 
 }
